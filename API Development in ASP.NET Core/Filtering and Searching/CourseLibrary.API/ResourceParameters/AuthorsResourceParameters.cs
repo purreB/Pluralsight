83320598725f7bd167acd51b9de354122ce0ea1d
Jakob Bergström <jakob.bergstrom@live.se>
2023-01-17 14:07:27 +0100

    added Filtering and Searching

---
+++ API Development in ASP.NET Core/Filtering and Searching/CourseLibrary.API/ResourceParameters/AuthorsResourceParameters.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.ResourceParameters
+{
+  public class AuthorsResourceParameters
+  {
+    public string Maincategory { get; set; }
+    public string SearchQuery { get; set; }
+  }
+}
