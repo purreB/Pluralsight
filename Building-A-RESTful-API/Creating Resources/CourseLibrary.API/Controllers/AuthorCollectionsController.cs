1e13f0b61cfb1a5cdd49a1af88d5dc2f59f7b6da
Jakob Bergström <jakob.bergstrom@live.se>
2023-01-17 15:54:05 +0100

    added creating authors collection route

--- Building-A-RESTful-API/Creating Resources/CourseLibrary.API/Controllers/AuthorCollectionsController.cs
+++ Building-A-RESTful-API/Creating Resources/CourseLibrary.API/Controllers/AuthorCollectionsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using CourseLibrary.API.Helpers;
 using CourseLibrary.API.Models;
 using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -22,6 +23,25 @@ namespace CourseLibrary.API.Controllers
       _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
     }
 
+    [HttpGet("({ids})", Name = "GetAuthorCollections")]
+    public IActionResult GetAuthorCollection([FromRoute][ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<Guid> ids)
+    {
+      if (ids == null)
+      {
+        return BadRequest();
+      }
+
+      var authorEntities = _courseLibraryRepository.GetAuthors(ids);
+
+      if (ids.Count() != authorEntities.Count())
+      {
+        return NotFound();
+      }
+
+      var authorsToReturn = _mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+      return Ok(authorsToReturn);
+    }
+
     [HttpPost]
     public ActionResult<IEnumerable<AuthorDto>> CreateAuthorCollection(IEnumerable<AuthorForCreationDto> authorCollection)
     {
@@ -33,7 +53,10 @@ namespace CourseLibrary.API.Controllers
 
       _courseLibraryRepository.Save();
 
-      return Ok();
+      var authorCollectionToReturn = _mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+      var idsAsString = string.Join(",", authorCollectionToReturn.Select(a => a.Id));
+      return CreatedAtRoute("GetAuthorCollections", new { ids = idsAsString }, authorCollectionToReturn);
+
     }
   }
 }
