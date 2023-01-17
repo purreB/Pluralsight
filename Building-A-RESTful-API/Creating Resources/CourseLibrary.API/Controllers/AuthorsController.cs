32746d66344e8824022a304cc8ec391d32977d75
Jakob Bergström <jakob.bergstrom@live.se>
2023-01-17 14:38:44 +0100

    added POST to authors

--- Building-A-RESTful-API/Creating Resources/CourseLibrary.API/Controllers/AuthorsController.cs
+++ Building-A-RESTful-API/Creating Resources/CourseLibrary.API/Controllers/AuthorsController.cs
@@ -11,42 +11,53 @@ using System.Threading.Tasks;
 
 namespace CourseLibrary.API.Controllers
 {
-    [ApiController]
-    [Route("api/authors")]
-    public class AuthorsController : ControllerBase
+  [ApiController]
+  [Route("api/authors")]
+  public class AuthorsController : ControllerBase
+  {
+    private readonly ICourseLibraryRepository _courseLibraryRepository;
+    private readonly IMapper _mapper;
+
+    public AuthorsController(ICourseLibraryRepository courseLibraryRepository,
+        IMapper mapper)
     {
-        private readonly ICourseLibraryRepository _courseLibraryRepository;
-        private readonly IMapper _mapper;
-
-        public AuthorsController(ICourseLibraryRepository courseLibraryRepository,
-            IMapper mapper)
-        {
-            _courseLibraryRepository = courseLibraryRepository ??
-                throw new ArgumentNullException(nameof(courseLibraryRepository));
-            _mapper = mapper ??
-                throw new ArgumentNullException(nameof(mapper));
-        }
-
-        [HttpGet()]
-        [HttpHead]
-        public ActionResult<IEnumerable<AuthorDto>> GetAuthors(
-            [FromQuery] AuthorsResourceParameters authorsResourceParameters)
-        {
-            var authorsFromRepo = _courseLibraryRepository.GetAuthors(authorsResourceParameters);
-            return Ok(_mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo));
-        }
-
-        [HttpGet("{authorId}")]
-        public IActionResult GetAuthor(Guid authorId)
-        {
-            var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
-
-            if (authorFromRepo == null)
-            {
-                return NotFound();
-            }
-             
-            return Ok(_mapper.Map<AuthorDto>(authorFromRepo));
-        }
+      _courseLibraryRepository = courseLibraryRepository ??
+          throw new ArgumentNullException(nameof(courseLibraryRepository));
+      _mapper = mapper ??
+          throw new ArgumentNullException(nameof(mapper));
     }
+
+    [HttpGet()]
+    [HttpHead]
+    public ActionResult<IEnumerable<AuthorDto>> GetAuthors(
+        [FromQuery] AuthorsResourceParameters authorsResourceParameters)
+    {
+      var authorsFromRepo = _courseLibraryRepository.GetAuthors(authorsResourceParameters);
+      return Ok(_mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo));
+    }
+
+    [HttpGet("{authorId}", Name = "GetAuthor")]
+    public IActionResult GetAuthor(Guid authorId)
+    {
+      var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+
+      if (authorFromRepo == null)
+      {
+        return NotFound();
+      }
+
+      return Ok(_mapper.Map<AuthorDto>(authorFromRepo));
+    }
+    [HttpPost]
+    public ActionResult<AuthorDto> CreateAuthor(AuthorForCreationDto author)
+    {
+      var authorEntry = _mapper.Map<Entities.Author>(author);
+      _courseLibraryRepository.AddAuthor(authorEntry);
+      _courseLibraryRepository.Save();
+      var authorToReturn = _mapper.Map<AuthorDto>(authorEntry);
+
+      return CreatedAtRoute("GetAuthor", new { authorId = authorToReturn.Id }, authorToReturn);
+
+    }
+  }
 }
--- Building-A-RESTful-API/Creating Resources/CourseLibrary.API/Profiles/AuthorsProfile.cs
+++ Building-A-RESTful-API/Creating Resources/CourseLibrary.API/Profiles/AuthorsProfile.cs
@@ -7,18 +7,19 @@ using System.Threading.Tasks;
 
 namespace CourseLibrary.API.Profiles
 {
-    public class AuthorsProfile : Profile
+  public class AuthorsProfile : Profile
+  {
+    public AuthorsProfile()
     {
-        public AuthorsProfile()
-        {
-            CreateMap<Entities.Author, Models.AuthorDto>()
-                .ForMember(
-                    dest => dest.Name, 
-                    opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
-                .ForMember(
-                    dest => dest.Age, 
-                    opt => opt.MapFrom(src => src.DateOfBirth.GetCurrentAge()));
-            ;
-        }
+      CreateMap<Entities.Author, Models.AuthorDto>()
+          .ForMember(
+              dest => dest.Name,
+              opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
+          .ForMember(
+              dest => dest.Age,
+              opt => opt.MapFrom(src => src.DateOfBirth.GetCurrentAge()));
+      ;
+      CreateMap<Models.AuthorForCreationDto, Entities.Author>();
     }
+  }
 }
