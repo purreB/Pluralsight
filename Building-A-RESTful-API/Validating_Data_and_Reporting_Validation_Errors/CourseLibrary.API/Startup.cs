01053be87faa7a56453d76fd37e163bd93d7f310
Jakob Bergström <jakob.bergstrom@live.se>
2023-01-18 11:41:06 +0100

    added correct status code

--- Building-A-RESTful-API/Validating_Data_and_Reporting_Validation_Errors/CourseLibrary.API/Startup.cs
+++ Building-A-RESTful-API/Validating_Data_and_Reporting_Validation_Errors/CourseLibrary.API/Startup.cs
@@ -12,63 +12,86 @@ using System;
 
 namespace CourseLibrary.API
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers(setupAction =>
-            {
-                setupAction.ReturnHttpNotAcceptable = true;
-
-            }).AddXmlDataContractSerializerFormatters();
-
-            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-            services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
-
-            services.AddDbContext<CourseLibraryContext>(options =>
-            {
-                options.UseSqlServer(
-                    @"Server=(localdb)\mssqllocaldb;Database=CourseLibraryDB;Trusted_Connection=True;");
-            }); 
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler(appBuilder =>
-                {
-                    appBuilder.Run(async context =>
-                    {
-                        context.Response.StatusCode = 500;
-                        await context.Response.WriteAsync("An unexpected fault happened. Try again later.");
-                    });
-                });
-
-            }
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddControllers(setupAction =>
+      {
+        setupAction.ReturnHttpNotAcceptable = true;
+
+      })
+      .AddXmlDataContractSerializerFormatters()
+      .ConfigureApiBehaviorOptions(setupAction =>
+      {
+        setupAction.InvalidModelStateResponseFactory = ContextBoundObject =>
+        {
+          var problemDetailsFactory = context.HttpContext.RequestServices.GetRequiredService<ProblemDetailsFactory>();
+          var problemDetails = problemDetailsFactory.CreateValidationProblemDetails(context.HttpContext, context.ModelState);
+
+          problemDetails.Detail = "See the errors field for details.";
+          problemDetails.Instance = context.HttpContext.Request.Path;
+
+          var actionExecutingContext = context as Microsoft.AspNetCore.Mvc.Filters.ActionExecutingContext;
+
+          if ((context.ModelState.ErrorCount > 0) && (actionExecutingContext?.ActionArguments.Count == context.ActionDescriptor.Parameters.Count))
+          {
+            problemDetails.Type = "https://courselibrary.com/modelvalidationproblem";
+            problemDetails.Status = StatusCodes.Status422UnprocessableEntity;
+            problemDetails.Title = "One or more validation errors occured.";
+          };
+        };
+
+      });
+
+
+      services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+      services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
+
+      services.AddDbContext<CourseLibraryContext>(options =>
+      {
+        options.UseSqlServer(
+                  @"Server=(localdb)\mssqllocaldb;Database=CourseLibraryDB;Trusted_Connection=True;");
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseExceptionHandler(appBuilder =>
+        {
+          appBuilder.Run(async context =>
+                  {
+                    context.Response.StatusCode = 500;
+                    await context.Response.WriteAsync("An unexpected fault happened. Try again later.");
+                  });
+        });
+
+      }
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
