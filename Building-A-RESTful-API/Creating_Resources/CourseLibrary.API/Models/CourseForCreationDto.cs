77666cf6d410b9c71b2fc71ed3f393e17a701483
Jakob Bergström <jakob.bergstrom@live.se>
2023-01-18 10:49:46 +0100

    Added some simple validation to CourseForCreationDto

--- Building-A-RESTful-API/Creating_Resources/CourseLibrary.API/Models/CourseForCreationDto.cs
+++ Building-A-RESTful-API/Creating_Resources/CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,10 @@ namespace CourseLibrary.API.Models
 {
   public class CourseForCreationDto
   {
+    [Required]
+    [MaxLength(100)]
     public string Title { get; set; }
-
+    [MaxLength(1500)]
     public string Description { get; set; }
 
     public Guid AuthorId { get; set; }
