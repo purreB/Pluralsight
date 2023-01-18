a5decb169bd352322902833fd4d02276d02344cb
Jakob Bergström <jakob.bergstrom@live.se>
2023-01-18 15:22:20 +0100

    added author delet method

--- Building-A-RESTful-API/Deleting_Resources/CourseLibrary.API/Controllers/AuthorsController.cs
+++ Building-A-RESTful-API/Deleting_Resources/CourseLibrary.API/Controllers/AuthorsController.cs
@@ -11,62 +11,75 @@ using System.Threading.Tasks;
 
 namespace CourseLibrary.API.Controllers
 {
-    [ApiController]
-    [Route("api/authors")]
-    public class AuthorsController : ControllerBase
+  [ApiController]
+  [Route("api/authors")]
+  public class AuthorsController : ControllerBase
+  {
+    private readonly ICourseLibraryRepository _courseLibraryRepository;
+    private readonly IMapper _mapper;
+
+    public AuthorsController(ICourseLibraryRepository courseLibraryRepository,
+        IMapper mapper)
     {
-        private readonly ICourseLibraryRepository _courseLibraryRepository;
-        private readonly IMapper _mapper;
-
-        public AuthorsController(ICourseLibraryRepository courseLibraryRepository,
-            IMapper mapper)
-        {
-            _courseLibraryRepository = courseLibraryRepository ??
-                throw new ArgumentNullException(nameof(courseLibraryRepository));
-            _mapper = mapper ??
-                throw new ArgumentNullException(nameof(mapper));
-        }
-
-        [HttpGet()]
-        [HttpHead]
-        public ActionResult<IEnumerable<AuthorDto>> GetAuthors(
-            [FromQuery] AuthorsResourceParameters authorsResourceParameters)
-        {
-            var authorsFromRepo = _courseLibraryRepository.GetAuthors(authorsResourceParameters);
-            return Ok(_mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo));
-        }
-
-        [HttpGet("{authorId}", Name ="GetAuthor")]
-        public IActionResult GetAuthor(Guid authorId)
-        {
-            var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
-
-            if (authorFromRepo == null)
-            {
-                return NotFound();
-            }
-             
-            return Ok(_mapper.Map<AuthorDto>(authorFromRepo));
-        }
-
-        [HttpPost]
-        public ActionResult<AuthorDto> CreateAuthor(AuthorForCreationDto author)
-        {
-            var authorEntity = _mapper.Map<Entities.Author>(author);
-            _courseLibraryRepository.AddAuthor(authorEntity);
-            _courseLibraryRepository.Save();
-
-            var authorToReturn = _mapper.Map<AuthorDto>(authorEntity);
-            return CreatedAtRoute("GetAuthor",
-                new { authorId = authorToReturn.Id },
-                authorToReturn);
-        }
-
-        [HttpOptions]
-        public IActionResult GetAuthorsOptions()
-        {
-            Response.Headers.Add("Allow", "GET,OPTIONS,POST");
-            return Ok();
-        }
+      _courseLibraryRepository = courseLibraryRepository ??
+          throw new ArgumentNullException(nameof(courseLibraryRepository));
+      _mapper = mapper ??
+          throw new ArgumentNullException(nameof(mapper));
     }
+
+    [HttpGet()]
+    [HttpHead]
+    public ActionResult<IEnumerable<AuthorDto>> GetAuthors(
+        [FromQuery] AuthorsResourceParameters authorsResourceParameters)
+    {
+      var authorsFromRepo = _courseLibraryRepository.GetAuthors(authorsResourceParameters);
+      return Ok(_mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo));
+    }
+
+    [HttpGet("{authorId}", Name = "GetAuthor")]
+    public IActionResult GetAuthor(Guid authorId)
+    {
+      var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+
+      if (authorFromRepo == null)
+      {
+        return NotFound();
+      }
+
+      return Ok(_mapper.Map<AuthorDto>(authorFromRepo));
+    }
+
+    [HttpPost]
+    public ActionResult<AuthorDto> CreateAuthor(AuthorForCreationDto author)
+    {
+      var authorEntity = _mapper.Map<Entities.Author>(author);
+      _courseLibraryRepository.AddAuthor(authorEntity);
+      _courseLibraryRepository.Save();
+
+      var authorToReturn = _mapper.Map<AuthorDto>(authorEntity);
+      return CreatedAtRoute("GetAuthor",
+          new { authorId = authorToReturn.Id },
+          authorToReturn);
+    }
+
+    [HttpOptions]
+    public IActionResult GetAuthorsOptions()
+    {
+      Response.Headers.Add("Allow", "GET,OPTIONS,POST");
+      return Ok();
+    }
+
+    [HttpDelete("{authorId}")]
+    public IActionResult DeleteAuthor(Guid authorId)
+    {
+      var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+      if (authorFromRepo == null)
+      {
+        return NotFound();
+      }
+      _courseLibraryRepository.DeleteAuthor(authorFromRepo);
+      _courseLibraryRepository.Save();
+      return NoContent();
+    }
+  }
 }
