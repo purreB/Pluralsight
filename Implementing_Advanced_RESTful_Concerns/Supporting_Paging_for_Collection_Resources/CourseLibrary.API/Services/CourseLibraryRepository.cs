113559826a79454adaeaae95057b6e463028c2bf
Jakob Bergström <jakob.bergstrom@live.se>
2023-01-19 09:58:20 +0100

    added PagedList helper class

--- Implementing_Advanced_RESTful_Concerns/Supporting_Paging_for_Collection_Resources/CourseLibrary.API/Services/CourseLibraryRepository.cs
+++ Implementing_Advanced_RESTful_Concerns/Supporting_Paging_for_Collection_Resources/CourseLibrary.API/Services/CourseLibraryRepository.cs
@@ -1,5 +1,6 @@
 ﻿using CourseLibrary.API.DbContexts;
 using CourseLibrary.API.Entities;
+using CourseLibrary.API.Helpers;
 using CourseLibrary.API.ResourceParameters;
 using System;
 using System.Collections.Generic;
@@ -123,7 +124,7 @@ namespace CourseLibrary.API.Services
       return _context.Authors.ToList<Author>();
     }
 
-    public IEnumerable<Author> GetAuthors(AuthorsResourceParameters authorsResourceParameters)
+    public PagedList<Author> GetAuthors(AuthorsResourceParameters authorsResourceParameters)
     {
       if (authorsResourceParameters == null)
       {
@@ -147,10 +148,8 @@ namespace CourseLibrary.API.Services
             || a.LastName.Contains(searchQuery));
       }
 
-      return collection
-      .Skip(authorsResourceParameters.PageSize * (authorsResourceParameters.PageNumber - 1))
-      .Take(authorsResourceParameters.PageSize)
-      .ToList();
+      return PagedList<Author>
+      .Create(collection, authorsResourceParameters.PageNumber, authorsResourceParameters.PageSize);
     }
 
     public IEnumerable<Author> GetAuthors(IEnumerable<Guid> authorIds)
