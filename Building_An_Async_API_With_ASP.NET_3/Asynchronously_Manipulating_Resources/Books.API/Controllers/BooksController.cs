7264a528d249787af2a0768f5feb07bb26434253
Jakob Bergström <jakob.bergstrom@live.se>
2023-01-24 10:16:49 +0100

    added new module

---
+++ Building_An_Async_API_With_ASP.NET_3/Asynchronously_Manipulating_Resources/Books.API/Controllers/BooksController.cs
@@ -0,0 +1,45 @@
+﻿using Books.API.Filters;
+using Books.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.API.Controllers
+{
+    [ApiController]
+    [Route("api/books")]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBooksRepository _booksRepository;
+
+        public BooksController(IBooksRepository booksRepository)
+        {
+            _booksRepository = booksRepository ?? 
+                throw new ArgumentNullException(nameof(booksRepository));
+        }
+
+        [HttpGet]
+        [BooksResultFilter]
+        public async Task<IActionResult> GetBooks()
+        {
+            var bookEntities = await _booksRepository.GetBooksAsync();
+            return Ok(bookEntities);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        [BookResultFilter]
+        public async Task<IActionResult> GetBook(Guid id)
+        {
+            var bookEntity = await _booksRepository.GetBookAsync(id);
+            if (bookEntity == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(bookEntity);
+        }
+    }
+}
