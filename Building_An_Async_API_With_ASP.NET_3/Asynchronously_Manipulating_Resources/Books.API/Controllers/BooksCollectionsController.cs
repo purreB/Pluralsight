e44f2d677e571ba6c2664f45587ba295a16d3a74
Jakob Bergström <jakob.bergstrom@live.se>
2023-01-24 10:57:14 +0100

    added functionality for CreateBookCollection

--- Building_An_Async_API_With_ASP.NET_3/Asynchronously_Manipulating_Resources/Books.API/Controllers/BooksCollectionsController.cs
+++ Building_An_Async_API_With_ASP.NET_3/Asynchronously_Manipulating_Resources/Books.API/Controllers/BooksCollectionsController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Books.API.Models;
 using Books.API.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -13,15 +14,25 @@ namespace Books.API.Controllers
   public class BooksCollectionsController : ControllerBase
   {
     private readonly IBooksRepository _booksRepository;
+    private readonly IMapper _mapper;
 
-    public BooksCollectionsController(IBooksRepository booksRepository)
+    public BooksCollectionsController(IBooksRepository booksRepository, IMapper mapper)
     {
       _booksRepository = booksRepository ?? throw new ArgumentNullException(nameof(booksRepository));
+      _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+
     }
 
     [HttpPost]
     public async Task<IActionResult> CreateBookCollection(IEnumerable<BookForCreation> bookCollection)
     {
+      var bookEntities = _mapper.Map<IEnumerable<Entities.Book>>(bookCollection);
+      foreach (var bookEntity in bookEntities)
+      {
+        _booksRepository.AddBook(bookEntity);
+      }
+
+      await _booksRepository.SaveChangesAsync();
       return Ok();
     }
   }
