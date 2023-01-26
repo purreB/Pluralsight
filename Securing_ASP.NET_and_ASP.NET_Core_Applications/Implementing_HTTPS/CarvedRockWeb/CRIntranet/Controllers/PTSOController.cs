3a92cf6a6e9482af30b75e17b2d13fdb946a9b6c
Jakob Bergström <jakob.bergstrom@live.se>
2023-01-26 08:56:52 +0100

    added new path and module

---
+++ Securing_ASP.NET_and_ASP.NET_Core_Applications/Implementing_HTTPS/CarvedRockWeb/CRIntranet/Controllers/PTSOController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRIntranet.Controllers
+{
+    public class PTSOController : Controller
+    {
+        // GET: PTSO
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string description, DateTime startDate, DateTime endDate)
+        {
+            return View();
+        }
+        
+    }
+}
